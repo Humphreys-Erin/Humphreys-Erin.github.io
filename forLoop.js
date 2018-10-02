895fe3af98b3d80b42dc541d35c62da59467492c
Humphreys-Erin <woo13069@byui.edu>
2018-10-02 18:50:44 -0400

    For Loop

---
+++ forLoop.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>JavaScript for loop</h2>
+
+<p>Humphreys-Erin.github.io</p>
+	<script>
+		var phrase = "";
+		var i;
+			for (i = 0; i < 7; i++){
+				phrase + "This number is " + i;
+			}
+		console.log(phrase);
+	</script>
+
+</body>
+</html>
