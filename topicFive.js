2152dc06c4b4543c833da2dad6504d709bc794b2
Humphreys-Erin <woo13069@byui.edu>
2018-11-10 14:34:53 -0500

    Add files via upload

---
+++ topicFive.js
@@ -0,0 +1,9 @@
+function saveFarmAnimal() {
+    var input = document.getElementById("farmAnimal");
+    localStorage.setItem("farmAnimal", input.value);
+}
+
+function getFarmAnimal() {
+    var pullData = localStorage.getItem("farmAnimal");
+    document.getElementById("data").innerHTML = "Your favorite farm animal: " + pullData;
+} 
