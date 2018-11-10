ab86de15d4a13652e8bdc5b06f3f0dc50682df2b
Humphreys-Erin <woo13069@byui.edu>
2018-11-10 14:33:57 -0500

    Delete topicFive.js

--- topicFive.js
+++
@@ -1,9 +0,0 @@
-function saveFarmAnimal() {
-    var input = document.getElementById("farmAnimal");
-    localStorage.setItem("farmAnimal", input.value);
-}
-
-function getFarmAnimal() {
-    var pullData = localStorage.getItem("farmAnimal");
-    document.getElementById("data").innerHTML = "Your favorite farm animal: " + pullData;
-}
